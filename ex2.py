51f766eca88c5a2667608d48cfc99b5b218f4b4f
Ann <31072029+annufka@users.noreply.github.com>
2018-07-06 23:15:50 +0300

    Update ex2.py

--- ex2.py
+++ ex2.py
@@ -1,1 +1,13 @@
+#Сортировка методом "пузырька"
+my_list = []
+n = 1
+for i in range(6):
+    a = int(input())
+    my_list.append(a)
+while n < len(my_list):
+    for j in range(1, 6):
+        if my_list[j] < my_list[j-1]:
+            my_list[j-1], my_list[j] = my_list[j], my_list[j-1]
+print(my_list)
 
+#
