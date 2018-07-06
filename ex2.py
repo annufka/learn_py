633082683ddf2b78cfbc813ba9f49c3b6a80cb9e
Ann <31072029+annufka@users.noreply.github.com>
2018-07-06 23:23:18 +0300

    Update ex2.py

--- ex2.py
+++ ex2.py
@@ -1,4 +1,4 @@
-#Сортировка методом "пузырька"
+#Сортировка методом "пузырька" с вводом с клавиатуры
 my_list = []
 n = 1
 for i in range(6):
@@ -10,4 +10,15 @@ while n < len(my_list):
             my_list[j-1], my_list[j] = my_list[j], my_list[j-1]
 print(my_list)
 
-#
+#Сортировка методом "пузырька" с random
+import random
+my_list = []
+n = 1
+for i in range(6):
+    a = random.randint(1, 10)
+    my_list.append(a)
+while n < len(my_list):
+    for j in range(1, 6):
+        if my_list[j] < my_list[j-1]:
+            my_list[j-1], my_list[j] = my_list[j], my_list[j-1]
+print(my_list)
