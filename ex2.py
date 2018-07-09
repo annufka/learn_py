d8ede3049fcc429b682ac7c6815744bc91019edc
Ann <31072029+annufka@users.noreply.github.com>
2018-07-09 13:21:22 +0300

    Update ex2.py

--- ex2.py
+++ ex2.py
@@ -22,3 +22,21 @@ while n < len(my_list):
         if my_list[j] < my_list[j-1]:
             my_list[j-1], my_list[j] = my_list[j], my_list[j-1]
 print(my_list)
+
+#сортировка выбором
+import random
+my_list = []
+n = 1
+for i in range(6):
+    a = random.randint(1, 10)
+    my_list.append(a)
+for k in range(len(my_list) - 1):
+        m = k
+        i = k + 1
+        while i < len(my_list):
+            if my_list[i] < my_list[m]:
+                m = i
+            i += 1
+        t = my_list[k]
+        my_list[k] = my_list[m]
+        my_list[m] = t
