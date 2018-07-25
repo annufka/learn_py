976c2ae1bd4ddfc425e0f0dd9e408b03b6f067ec
Ann <31072029+annufka@users.noreply.github.com>
2018-07-25 11:27:24 +0300

    Update number.py

--- number.py
+++ number.py
@@ -1,2 +1,19 @@
 #Угадай число
+import random
+WIN = False
+computer = random.randint(1, 50)
+def compare(computer, men):
+    if computer == men:
+        print("You are winner!")
+        WIN = True
+    if computer > men:
+        print("My number is greater...")
+        WIN = False
+    if computer < men:
+        print("My number is less...")
+        WIN = False
+    return WIN
 
+while WIN == False:
+    men = int(input("Input your number"))
+    compare(computer, men)
