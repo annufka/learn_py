7d9f0168ab31963fb1bd4d089b7e238fd6fa4623
Ann <31072029+annufka@users.noreply.github.com>
2019-02-15 20:58:09 +0200

    Update Person.py

--- Person.py
+++ Person.py
@@ -1,11 +1,12 @@
 class Person():
-    def __init__(self, memory=[]):
+    def __init__(self, name, age, memory=[]):
+        self.name = name
+        self.age = age
         self.memory = memory
 
     def know(self, person):
         if person not in self.memory:
             self.memory.append(person)
-            person.memory.append(self)
         return self.memory
 
     def is_known(self, person):
@@ -13,7 +14,7 @@ class Person():
             return False
         return True
 
-pete = Person()
-kate = Person()
+pete = Person('Pete', 23)
+kate = Person('Kate', 19)
 pete.know(kate)
 print(kate.is_known(pete))
